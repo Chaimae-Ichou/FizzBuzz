bfd25e91833051e80a598b614075fd8438e15c66
Chaimae-Ichou <ichouchaimae4gmail.com>
2025-12-15 03:22:21 +0100

    Refactoring

--- src/com/controls/td6/FizzBuzz.java
+++ src/com/controls/td6/FizzBuzz.java
@@ -2,16 +2,19 @@ package com.controls.td6;
 
 public class FizzBuzz {
     public static String de(int nombre){
-        if (nombre % 5 == 0 && nombre % 3 == 0) {
+        boolean divisiblePar3 = nombre % 3 == 0;
+        boolean divisiblePar5 = nombre % 5 == 0;
+
+        if (divisiblePar3 && divisiblePar5) {
             return "FizzBuzz";
         }
-        if (nombre % 5 == 0) {
-            return "Buzz";
-        }
-        if (nombre % 3 == 0) {
+        if (divisiblePar3) {
             return "Fizz";
         }
+        if (divisiblePar5) {
+            return "Buzz";
+        }
+        return Integer.toString(nombre);
 
-        return String.valueOf(nombre);
     }
 }
